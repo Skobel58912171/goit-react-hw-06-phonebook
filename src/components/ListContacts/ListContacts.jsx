5d51e744d1cdc348af7ef0d3765327c1d4ac1d76
Yuliia Skobel <106476498+Skobel58912171@users.noreply.github.com>
2023-02-23 20:19:42 +0200

    finish

--- src/components/ListContacts/ListContacts.jsx
+++ src/components/ListContacts/ListContacts.jsx
@@ -1,6 +1,4 @@
-import ItemContact from 'components/ItemContact/ItemContact';
-import { Item } from './ListContacts.styled';
-import { List } from './ListContacts.styled';
+import { Item, List, DataContact, BtnDelete } from './ListContacts.styled';
 import { useSelector, useDispatch } from 'react-redux';
 import { getFilteredContacts } from 'redux/selector';
 import { deleteContact } from 'redux/contactsSlice';
@@ -10,16 +8,19 @@ const ListContacts = () => {
   const contacts = useSelector(getFilteredContacts);
   return (
     <List>
-      {contacts.map(contact => (
-        <Item key={contact.id}>
-          <ItemContact
-            id={contact.id}
-            name={contact.name}
-            number={contact.number}
-            onDelete={() => {
-              dispatch(deleteContact(contact.id));
+      {contacts.map(({ id, name, number }) => (
+        <Item key={id}>
+          <DataContact>
+            <span>{name}</span>: <span>{number}</span>
+          </DataContact>
+          <BtnDelete
+            type="button"
+            onClick={() => {
+              dispatch(deleteContact(id));
             }}
-          />
+          >
+            Delete
+          </BtnDelete>
         </Item>
       ))}
     </List>
--- src/redux/selector.js
+++ src/redux/selector.js
@@ -1,9 +1,6 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-export const getContacts = state => {
-  console.log(state);
-  return state.contacts;
-};
+export const getContacts = state => state.contacts;
 
 export const getFilter = state => state.filter;
 
